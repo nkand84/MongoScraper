082cddada3ef42f20532508bc27811058e1d19c5
nkand84 <nityabhagi@gmail.com>
2018-05-06 15:37:49 -0700

    no duplicates, displays on click

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,26 +1,28 @@
-$("#scrape-article").on("click", function (e) {
-    e.preventDefault();
-    $.getJSON("/all", function (data) {
-        displayResults(data);
+$(document).ready(function () {
+    $("#scrape-article").on("click", function (e) {
+        e.preventDefault();
+        $.getJSON("/articles", function (data) {
+            displayResults(data);
+        });
+
+
     });
-    
 
-});
+    function displayResults(data) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+            var div = $("<div class='panel panel-default'>");
+            var divHead = $("<div class='panel-heading'>");
+            var divTitle = $("<h3 class='panel-title'>");
+            var divBody = $("<div class='panel-body'>");
+            divTitle.text(data[i].title);
+            divBody.text(data[i].summary);
+            divHead.append(divTitle);
+            div.append(divHead);
+            div.append(divBody);
+            $("#scrape-content").append(div);
+        }
 
-function displayResults(data) {
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-        var div = $("<div class='panel panel-default'>");
-        var divHead = $("<div class='panel-heading'>");
-        var divTitle = $("<h3 class='panel-title'>");
-        var divBody = $("<div class='panel-body'>");
-        divTitle.text(data[i].title);
-        divBody.text(data[i].summary);
-        divHead.append(divTitle);
-        div.append(divHead);
-        div.append(divBody);
-        $("#scrape-content").append(div);
     }
-
-}
+});
 
